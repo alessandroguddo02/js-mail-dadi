964e1ab655a19c212204481f69be6228b53eb9a6
alessandroguddo02 <guddoalessandro6@gmail.com>
2024-11-04 17:38:12 +0100

    second exercise

--- mail/script.js
+++ mail/script.js
@@ -33,3 +33,16 @@ Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 Stabilire il vincitore, in base a chi fa il punteggio più alto.
 */
 
+let numPc = Math.floor((Math.random() * 6) + 1);
+let numUtente = Math.floor((Math.random() * 6) + 1);
+console.log(numPc)
+console.log(numUtente)
+
+if(numPc < numUtente){
+    console.log("hai vinto");
+}else if(numPc > numUtente){
+    console.log("hai perso");
+}else{
+    console.log("pareggio");
+}
+
