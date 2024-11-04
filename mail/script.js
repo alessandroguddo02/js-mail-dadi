8d6c3514177c83d55be2c868bcf05ff4014393bd
alessandroguddo02 <guddoalessandro6@gmail.com>
2024-11-04 17:37:34 +0100

    first exercise

---
+++ mail/script.js
@@ -0,0 +1,35 @@
+/*Mail
+Chiedi all’utente la sua email,
+controlla che sia nella lista di chi può accedere,
+stampa un messaggio appropriato sull’esito del controllo.
+Non è necessario provvedere alla validazione delle email
+*/
+"use strict"
+console.clear();
+
+let mails = ["alessandro@gmail.com", "pippo@gmail.com", "pluto@gmail.com"];
+let richiesta = prompt("inserisci la tua mail");
+console.log(richiesta);
+let registrato = false
+for(let i = 0; i < mails.length; i++){
+    console.log(i);
+    if(mails[i] === richiesta){
+        //console.log("la tua mail è registrata");
+        registrato = true;
+    }
+}
+
+if(registrato){
+    console.log("la tua mail è registrata")
+}else{
+    console.log("la tua mail non è registrata")
+}
+
+
+
+/*
+Gioco dei dadi
+Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+Stabilire il vincitore, in base a chi fa il punteggio più alto.
+*/
+
